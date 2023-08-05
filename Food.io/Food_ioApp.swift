35a9c969df76ca1eb1e8f031649012bbed263395
Naomi <naomipremlim@gmail.com>
2023-08-05 01:39:57 -0700

    Initial Commit

---
+++ Food.io/Food_ioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_ioApp.swift
+//  Food.io
+//
+//  Created by Naomi on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Food_ioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
